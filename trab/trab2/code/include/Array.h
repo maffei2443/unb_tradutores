d4575be1cbe47a63aa74fa9b34660b3abe8fafca
maffei2443 <leoitu22hotmail.com@gmail.com>
2019-09-09 23:37:52 -0300

    organizando; makefile

---
+++ trab/trab2/code/include/Array.h
@@ -0,0 +1,40 @@
+// Agradecimentos
+#pragma once
+// https://stackoverflow.com/questions/3536153/c-dynamically-growing-array
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+  char* array;
+  size_t used;
+  size_t size;
+} Array;
+
+
+void Array_Init(Array * a, size_t initSize);
+void Array_Insert(Array * a, char * other);
+void Array_Delete(Array* a);
+
+
+
+// int main() {
+//   Array buf;
+//   Array_Init(&buf, 20);
+//   while(1) {
+//     char * pt;
+//     pt = malloc(sizeof(char) * 20);
+//     scanf("%s", pt);
+//     printf("%s\n", pt);
+//     Array_Insert(&buf, pt);
+//     printf("Sum: %s\n", buf.array);
+//     if (!strcmp("quit", pt)){
+//       pt = NULL;
+//       free(pt);
+//       break;
+//     }
+//     free(pt);
+//   }
+//   Array_Delete(&buf);
+// }
