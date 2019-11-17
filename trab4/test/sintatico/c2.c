459fecfa3526e85c2ed7c442a4ffdc639e23ac38
maffei2443 <leoitu22hotmail.com@gmail.com>
2019-11-17 01:03:09 -0300

    renomeando arquivos de teste corretos do sintático

--- trab4/test/sintatico/c2.c
+++ trab4/test/sintatico/c2.c
@@ -5,14 +5,14 @@ int foo() {
 }
 
 int arr[4];
-mat int m[1][1] = [{1}];
+mat int m[1][1] = {{1}};
 
 int main() {
 	int a[3] = {1, 2, 3};
-	mat int fm[3][3] = [
+	mat int fm[3][3] = {
 		{1, 2, 3}
 		{4, 5, 6}
-		{7, 8, 9}];
+		{7, 8, 9}};
 	int top = a + 12 * 17 % (9 - &fm);
 	return a[2];
 }
